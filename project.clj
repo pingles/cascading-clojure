6183f19e7ced1983b5eed0f666224f621842d4ab
Paul Ingles <paul@oobaloo.co.uk>
2011-03-16 10:00:07 +0000

    updated releases

--- project.clj
+++ project.clj
@@ -3,13 +3,13 @@
   :java-source-path "src/jvm"
   :java-fork "true"
   :dependencies
-    [[org.clojure/clojure "1.2.0-RC3"]
-     [org.clojure/clojure-contrib "1.2.0-RC3"]
+    [[org.clojure/clojure "1.2.0"]
+     [org.clojure/clojure-contrib "1.2.0"]
      [cascading "1.0.17-SNAPSHOT" :exclusions [javax.mail/mail janino/janino]]
-     [clj-json "0.3.0-SNAPSHOT"]
+     [clj-json "0.3.1"]
      [clj-serializer "0.1.1"]]
   :dev-dependencies
     [[lein-javac "1.2.1-SNAPSHOT"]]
-  :namespaces
+  :aot
     [cascading.clojure.api
      cascading.clojure.testing])
